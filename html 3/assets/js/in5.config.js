2b1334accfe7dd5b7c0e3ae174dd3da6c7462296
RVPMedia <no mail>
2013-04-25 19:39:18 +0100

    update

--- html 3/assets/js/in5.config.js
+++ html 3/assets/js/in5.config.js
@@ -217,7 +217,7 @@ $(function(){
 		}
 	});
 	if(multifile){
-		nav = { numPages:8,
+		nav = { numPages:9,
 		current:parseInt(location.href.split('/').pop().split('.html').join('')),
 		back:function(ref){nav.to(nav.current-1);},
 		next:function(ref){nav.to(nav.current+1);},
